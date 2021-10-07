5970f9200528af3c0e7d695c7109814c9d9f97ac
Kazae67 <kazae.zaeka@gmail.com>
2021-10-07 14:55:21 +0200

    init

---
+++ register.php
@@ -0,0 +1,13 @@
+
+<html>
+  <head>
+    <title>Inscrivez-vous</title>
+  </head>
+<body>
+    <form method="post" action="controller.php">
+        Choisissez votre pseudo : <input type="text" name="nom" placeholder="Entrez votre pseudo: " /><br />
+        Choisissez votre email : <input type="email" name="email" placeholder="Entrer votre Email : " /><br />
+        <input type="submit" value="Valider" />
+    </form>
+</body>
+</html>
